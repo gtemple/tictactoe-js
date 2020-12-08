13231263eb3b430f575ad4535bc8bfba5ebdaced
Giordano <giordanotemple@gmail.com>
2020-12-08 08:31:36 -0500

    added win conditions, further updated stylings

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/* 
+FUNCTION NAMES AND USES
+
+    gameboard(): creates the new board and prints in window
+        newGame(): clears old board adds 9 empty divs
+        printboard(): prints in 3x3 format
+
+    player(): creates new player with name, pieces, and score starting at 0
+
+    playGame(): begins new game
+        - creates new gameboard
+        nameInput(): creates new player
+
+
+    
+*/
+
 let htmlBoard = document.querySelector('.board');
 
 const gameBoard = () => {
@@ -6,7 +23,7 @@ const gameBoard = () => {
     const newGame = () => {
         board = []
         while (board.length < 9) {
-            board.push('s')
+            board.push(' ')
         };
         console.log(board)
     };
@@ -32,12 +49,14 @@ const player = () => {
     piece = '';
     name = '';
 
-    return {piece, name};
+    return {score, piece, name};
 }
 
 const playGame = () => {
     let g = gameBoard();
     g.printBoard();
+
+    let winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
     let nameInput = (n) => {
         let p = prompt(`Player ${n}, insert your name`, '')
@@ -45,17 +64,26 @@ const playGame = () => {
         return p;
     };
 
+    let turnSwitch = (p) => {
+        return `${p}, it's your turn! Click on an empty space in the grid to place your piece.`;
+    };
+
     let p1 = player();
     p1.piece = 'x';
     p1.name = nameInput(1);
-    document.getElementById("player1").innerHTML = p1.name
+    document.getElementById("player1").innerHTML = p1.name;
     let p2 = player()
     p2.piece = 'o';
     p2.name = nameInput(2);
-    document.getElementById("player2").innerHTML = p2.name
-    
-    console.log(p1)
+    document.getElementById("player2").innerHTML = p2.name;
 
+
+    let beginRound = (playerA, playerB) => {
+         turnSwitch(playerA.name);
+         turnSwitch(playerB.name)
+    }
+
+    playRound(p1, p2);
 };
 
 let ng = playGame()
